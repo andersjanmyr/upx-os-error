1f3e60f2c0d39a6f3e5a2a2b9f3ce2abee249da4
Anders Janmyr <anders@janmyr.com>
2019-12-09 14:50:59 +0100

    Fixed typo

--- main.go
+++ main.go
@@ -12,6 +12,6 @@ func main() {
 	sess := session.Must(session.NewSession(&aws.Config{
 		Region: aws.String("eu-west-1"),
 	}))
-	s3 := s3.New(sess)
-	fmt.Println(s3.ListBuckets())
+	sThree := s3.New(sess)
+	fmt.Println(sThree.ListBuckets(&s3.ListBucketsInput{}))
 }
